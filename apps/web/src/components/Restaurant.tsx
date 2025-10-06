747c7385b87250e265c27ad6de26a90931f6dcf8
niney <kpeteras@gmail.com>
2025-10-06 18:28:30 +0900

    [web] RestaurantMobileWeb 경로 구조 정리 및 컴포넌트 참조 경로 수정

--- apps/web/src/components/Restaurant.tsx
+++ apps/web/src/components/Restaurant.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import RestaurantDesktop from './RestaurantDesktop'
-import RestaurantMobileWeb from './RestaurantMobileWeb'
+import RestaurantMobileWeb from './RestaurantMobileWeb/RestaurantMobileWeb'
 import { useRestaurant } from '../hooks/useRestaurant'
 
 interface RestaurantProps {
